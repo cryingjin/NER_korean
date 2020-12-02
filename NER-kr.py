9a7063a7e7c65a4fe818706b849fccbb84479112
HyunLee <chdnjf103@gmail.com>
2020-12-02 18:48:45 +0900

    updt concat other features

--- NER-kr.py
+++ NER-kr.py
@@ -7,16 +7,27 @@ from sklearn.cluster import DBSCAN
 import seaborn as sns
 from tensorflow.keras.utils import to_categorical
 import numpy as np
+import pandas as pd
+import collections
+# import pyLDAvis
+# from kmeans_visualizer import kmeans_to_prepared_data
+
 
 # load token(형태소 단위)
 with open('token/train_token.pickle','rb') as fr:
     train_token = pickle.load(fr)
 
 # load glove embedding vector(형태소)
-with open('token/train_token.pickle','rb') as fr:
-    train_token = pickle.load(fr)
+with open('emb_word_dict_1202 (1).pickle','rb') as fr:
+    embedding = pickle.load(fr)
 
-def mk_cluster_feature(token,embedding=None,w2v=True,vector_size=128,km=True,dbscan=False,show=True,one_hot=True):
+# load 개체명 사전 vector(형태소)
+with open('nedict_onehot.pickle','rb') as fr:
+    nerdict = pickle.load(fr)
+
+
+
+def mk_cluster_feature(token,embedding=None,w2v=True,vector_size=128,km=True,dbscan=False,show=True,one_hot=True,num_cluster=5):
     """
     embedding : 29084 x vector_size(128), np array
     -> only for using extenal embedding vector
@@ -26,16 +37,18 @@ def mk_cluster_feature(token,embedding=None,w2v=True,vector_size=128,km=True,dbs
         model = Word2Vec(sentences=token,size=vector_size,window=10,sg=1,min_count=1,workers=-1,iter=20)
         embedding = model.wv.vectors
         print(f"embedding shape :{embedding.shape}") # unique token 29084
-    else:
-        embedding = embedding
+    else: # using glove 
+        embedding_vector = np.array(list(embedding.values()))
+        embedding_key = list(embedding.keys())
 
     # K-means
     if km:
-        kmeans = KMeans(n_clusters=11) # target class 갯수만큼 clustering
-        kmeans.fit(embedding)
-        cluster = kmeans.predict(embedding)
-        names = model.wv.index2word
-        word_cluster_map = {names[i]: cluster[i] for i in range(len(names))}
+        kmeans = KMeans(n_clusters=num_cluster,random_state=2020) # target class 갯수만큼 clustering
+        kmeans.fit(embedding_vector)
+        cluster = kmeans.predict(embedding_vector)
+        print(collections.Counter(cluster))
+        label = kmeans.labels_
+        word_cluster_map = {embedding_key[i]: cluster[i] for i in range(len(embedding_key))}
 
     # DBSCAN
     if dbscan:
@@ -44,30 +57,46 @@ def mk_cluster_feature(token,embedding=None,w2v=True,vector_size=128,km=True,dbs
         names = model.wv.index2word
         word_cluster_map = {names[i]: cluster[i] for i in range(len(names))}
 
-    cluster = cluster[:,np.newaxis]
-    data = np.concatenate((embedding,cluster),axis=1)
-
     # Viz
     if show:
-        visualize(data,type='tsne')
-        for cluster in range(0,10):
-            print("\nCluster {}".format(cluster))
+        visualize(embedding_vector,label,type='pca')
+        # visualize(embedding,label,type='tsne')
+
+        for clusters in range(0,6):
+            print("\nCluster {}".format(clusters))
             words = []
-            for i in range(0,100): 
-                if( list(word_cluster_map.values())[i] == cluster ):
+            for i in np.random.randint(0, 10000, size=100): 
+                if( list(word_cluster_map.values())[i] == clusters ):
                     words.append(list(word_cluster_map.keys())[i])
             print(words)
 
     # mk cluster feature vector
     cluster_feature = []
+    ner_feature = []
+    pos_feature = []
+
     if one_hot:
         cluster = to_categorical(cluster)
-    for sen in token:
-        for w in sen:
-            cluster_feature.append(list(word_cluster_map[str(w)]))
+        
+    word_cluster_map2 = {embedding_key[i]: cluster[i] for i in range(len(embedding_key))}
 
-    return cluster_feature
+    # 유니크한 각 형태소에 대해 feature 생성 및 합치기
+    for w in embedding_key:
+        cluster_feature.append(list(word_cluster_map2[str(w)]))
+        ner_feature.append(list(ner_dict[str(w)]))
+        pos_feature.append(list(pos_dict[str(w)]))
 
+    
+    cluster_feature = np.array(cluster_feature)
+    ner_feature = np.array(ner_feature)
+    pos_feature = np.array(pos_feature)
 
+    print(cluster_feature.shape,ner_feature.shape,pos_feature.shape)
 
-tem = mk_cluster_feature(train_token)
+    res = np.concatenate((embedding_vector,cluster_feature,ner_feature,pos_feature),axis=1)
+
+    total_feature_dict = {embedding_key[i] : res[i] for i in range(len(embedding_key))}
+
+    return total_feature_dict
+
+total = mk_cluster_feature(train_token, embedding=embedding,w2v=False,num_cluster=6)
--- utils.py
+++ utils.py
@@ -10,33 +10,38 @@ from sklearn.manifold import TSNE
 import random
 import numpy as np
 
-def visualize(data,type='tsne'):
+def visualize(data,label,type='tsne'):
     fig = plt.figure(figsize=(12, 8))
     ax = Axes3D(fig)
 
-    sample = random.sample(list(data),2000)
-    print(np.array(sample).shape)
+    # sample = random.sample(list(data),2000)
+    # print(np.array(sample).shape)
 
-    vector = np.array(sample)[:,:128]
-    label = np.array(sample)[:,128:]
+    # vector = np.array(sample)[:,:128]
+    # label = np.array(sample)[:,128:]
 
     if type == 'tsne':
-        tsne = TSNE(n_components=3) 
-        tsne_results = tsne.fit_transform(vector)
-        ax.scatter(tsne_results[:,0], tsne_results[:,1],tsne_results[:,2], s=50,c=label, edgecolors='white')
-        ax.set_title('T-SNE of distribution')
-        ax.set_xlabel('tsne1')
-        ax.set_ylabel('tsne2')
-        ax.set_zlabel('tsne3')
+        tsne = TSNE(n_components=2) 
+        tsne_results = tsne.fit_transform(data)
+        xs = tsne_results[:,0]
+        ys = tsne_results[:,1]
+        plt.scatter(xs,ys,c=label)
         plt.show()
+    
+        # ax.scatter(tsne_results[:,0], tsne_results[:,1], s=50,c=label, edgecolors='white')
+        # ax.set_title('T-SNE of distribution')/
+        # ax.set_xlabel('tsne1')
+        # ax.set_ylabel('tsne2')
+        # plt.show()
 
 
     else:
         pca3 = PCA(n_components=3)
-        data_pca3 = pca3.fit_transform(vector)
+        data_pca3 = pca3.fit_transform(data)
         ax.scatter(data_pca3[:,0], data_pca3[:,1],data_pca3[:,2], s=50,c=label, edgecolors='white')
         ax.set_title('PCA of Target distribution')
         ax.set_xlabel('pc1')
         ax.set_ylabel('pc2')
         ax.set_zlabel('pc3')
         plt.show()
+
